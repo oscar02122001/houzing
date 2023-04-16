1fab5a97de9ebbf5d255bde3ebabd5b4cbf5e9c5
oscar02122001 <olimjonismatov01@gmail.com>
2023-04-16 05:31:36 +0500

    intro slide component

---
+++ src/components/intro/slide.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./style";
+import "../../../src/index.css";
+import {
+  Image,
+  Wrapper,
+  Title,
+  MinTitle,
+  Price,
+  Text,
+  Button,
+  WrapIcon,
+  IconBath,
+  IconBed,
+  IconGarage,
+  IconSquar,
+} from "./style";
+class Slides extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <Wrapper className="flexCentre">
+        <Image src={data.background} />
+        <Title>{data.title}</Title>
+        <Text>{data.text}</Text>
+        <WrapIcon>
+          <div className="icon-wrap">
+            <IconBed />
+            <Text>{data.bed} Beds</Text>
+          </div>
+          <div className="icon-wrap">
+            <IconBath />
+            <Text>{data.bath} Baths</Text>
+          </div>
+          <div className="icon-wrap">
+            <IconGarage />
+            <Text>{data.g} Garage</Text>
+          </div>
+          <div className="icon-wrap">
+            <IconSquar />
+            <Text>{data.s} Sq Ft</Text>
+          </div>
+        </WrapIcon>
+        <Price>${data.price}/mo</Price>
+        <Button>Read more</Button>
+      </Wrapper>
+    );
+  }
+}
+
+export default Slides;
