8cc12d29ae1c20ce0579b44443f5a621687157b3
oscar02122001 <olimjonismatov01@gmail.com>
2023-04-25 13:35:59 +0500

    choose full

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import Recommend from "../recommend/recommend";
 import Headerrr from "../header/header.jsx";
 import Chose from "../choses/chose.jsx";
 import Intro from "../intro/intro";
+import Category from "../category/category.jsx";
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <Intro />
         <Recommend />
         <Chose />
+        <Category />
       </div>
     );
   }
